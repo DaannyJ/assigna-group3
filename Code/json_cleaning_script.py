ede4fa78569f894eef0ee043ae89133afc1dda3c
Daniel Johansson <daniel.johsnsson@gmail.com>
2023-05-10 11:55:37 +0200

    Added json script

---
+++ Code/json_cleaning_script.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import json
+
+# Load the JSON data into a pandas DataFrame
+with open('json_files/2006.json') as f:
+    data = json.load(f)
+df = pd.json_normalize(data)
+
+# Select the columns you want to keep
+df = df[['headline', 'last_publication_date', 'publication_date',
+         'description.conditions', 'description.text',
+         'duration.label', 'working_hours_type.label',
+         'workplace_address.city', 'workplace_address.municipality_code']]
+
+# Remove rows with NaN/NULL values
+df = df.dropna()
+
+# Print the resulting DataFrame
+df
