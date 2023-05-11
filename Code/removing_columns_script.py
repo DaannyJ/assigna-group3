f08de62aa435c7c294fd842124beefb9e9fc723a
casamu <gussamcag@student.gu.se>
2023-05-11 12:30:03 +0200

    Removed cols with a lot of null nan. Dropping rest of null nan

--- Code/removing_columns_script.py
+++ Code/removing_columns_script.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 # Read in the CSV file
-df = pd.read_csv("INSERTYEAR_cleaned.csv")
+df = pd.read_csv('DIN FILEPATH HÄR')
 
 # Select only the desired columns
-df_selected = df[['application_deadline', 'headline', 'last_publication_date',
-                  'publication_date', 'description.conditions', 'description.text',
-                  'duration.label', 'working_hours_type.label',
-                  'workplace_address.city', 'workplace_address.municipality_code']]
+df_selected = df[['headline','publication_date', 'description.text',
+                  'workplace_address.municipality_code']]
 
-# Print the new dataframe with only the selected columns
-df_selected.to_csv("INSERTYEAR_cleaned_correct.csv")
+
+# drop all rows where there are null and nan
+df = df.dropna()
+print(df.head)
+
+# skriver till ny csv fil
+df_selected.to_csv("FILNAMN")
