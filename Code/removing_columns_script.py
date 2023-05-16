7f8d4014ed49a1139deca865773622793b5e0665
casamu <gussamcag@student.gu.se>
2023-05-16 11:59:06 +0200

    Fixed variable name confusion

--- Code/removing_columns_script.py
+++ Code/removing_columns_script.py
@@ -4,12 +4,12 @@ import pandas as pd
 df = pd.read_csv('DIN FILEPATH HÄR')
 
 # Select only the desired columns
-df_selected = df[['headline','publication_date', 'description.text']]
+df = df[['headline','publication_date', 'description.text']]
 
 
 # drop all rows where there are null and nan
-df_selected = df.dropna()
+df = df.dropna()
 print(df.head)
 
 # skriver till ny csv fil
-df_selected.to_csv("FILNAMN.csv")
+df.to_csv("FILNAMN.csv")
