fde4e339080c2c166c0b51dcaabce2c67d5296e0
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-05-17 13:48:23 +0200

    Update most_common_occurring.py
    it works now

--- Code/most_common_occurring.py
+++ Code/most_common_occurring.py
@@ -2,9 +2,7 @@ from collections import Counter
 from nltk import ngrams
 import pandas as pd
 
-data = pd.read_csv('C:/Users/carlt/Documents/TIG326/kod till projektet/assigna-group3/code/preprocessed_swe_1.csv', index_col=0)
-
-
+data = pd.read_csv('C:/Users/jonat/Documents/GitHub/assigna-group3/Code/preprocessed_swe_1.csv', index_col=0)
 
 def get_top_grams(df, row_limit, num_grams):
     # Limit the DataFrame to the specified number of rows
@@ -14,20 +12,9 @@ def get_top_grams(df, row_limit, num_grams):
     all_grams = []
 
     # Extract monograms and bigrams from tokenized descriptions
-    # for desc in df['description']:
-    #     monograms = desc
-    #     bigrams = list(ngrams(desc, 2, pad_left=True, pad_right=True))
-    #     all_grams.extend(monograms + bigrams)
-
-
     for desc in df['description']:
-        monograms = desc
-        bigrams = list(ngrams(desc, 2, pad_left=True, pad_right=True))
-        all_grams.extend(monograms)
-        all_grams.extend(bigrams)
-
-    # Count the occurrences of each monogram and bigram
-
+        grams = desc.split()
+        all_grams.extend(grams)
     # Count the occurrences of each monogram and bigram
     gram_counts = Counter(all_grams)
 
@@ -54,48 +41,5 @@ top_grams = get_top_grams(df, row_limit, num_grams)
 
 # Print the results
 for gram, count in top_grams:
-    print(gram, ":", count)
-'''
-
-
-##GAMLA##
-'''
-import pandas as pd
-import nltk
-from collections import Counter
-from nltk import ngrams
-from nltk.tokenize import word_tokenize
-
-
-# Load the dataset into a pandas DataFrame
-df = pd.read_csv('fil här')
-#print(df.head)
-# Set the desired row limit
-row_limit = 10000
-
-# Limit the DataFrame to the specified number of rows
-df = df.head(row_limit)
-
-# Tokenize each description
-tokenized_descriptions = [word_tokenize(desc.lower()) for desc in df['description.text']]
-
-# Create a list to store all monograms and bigrams
-all_grams = []
-
-# Extract monograms and bigrams from tokenized descriptions
-for desc in tokenized_descriptions:
-    monograms = desc
-    bigrams = list(ngrams(desc, 2, pad_left=True, pad_right=True))
-    all_grams.extend(monograms + bigrams)
-
-# Count the occurrences of each monogram and bigram
-# ändrat från allgrams till bara bigrams för att se
-gram_counts = Counter(all_grams)
-
-# Get the most common 10 monograms and bigrams
-top_10_grams = gram_counts.most_common(100)
-
-# Print the results
-for gram, count in top_10_grams:
     print(gram, ":", count)
 '''
