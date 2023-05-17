befe92c5048fd3b322cbfa97a2800ce0a94ddd87
DaannyJ <daniel.johsnsson@gmail.com>
2023-05-17 10:23:47 +0200

    Yes

--- Code/streamlit_app.py
+++ Code/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -16,7 +17,7 @@ industry = st.sidebar.selectbox(
     "Industry",
     ("Private Sector", "Public Sector")
 )
-#Hej
+
 # Define the text input or file upload option
 option = st.sidebar.radio(
     "Choose an option:",
@@ -31,14 +32,6 @@ def get_cosine_similarity_score(text1, text2):
     similarity_scores = cosine_similarity(X)
     return similarity_scores[0][1]
 
-# Define the function to search for matches in the list
-def search_keywords(keyword):
-    matches = []
-    for item in data:
-        if keyword.lower() in item[0].lower():
-            matches.append(item[0])
-    return matches
-
 # Define the main content area
 st.markdown("### Enter or Upload Job Ad Text")
 if option == "Input Text":
@@ -50,63 +43,61 @@ else:
     else:
         st.warning("Please upload a file or enter text.")
 
-search_input = st.text_input("Search for a job title")
-
-# Define the list of buzzwords and phrases
-data = [
-    ['Undersköterskor, hemtjänst, hemsjukvård och äldreboende'],
-    ['Grundskollärare'],
-    ['Butikssäljare, fackhandel'],
-    ['Lager- och terminalpersonal'],
-    ['Mjukvaru- och systemutvecklare m.fl.'],
-    ['Barnskötare'],
-    ['Butikssäljare, dagligvaror'],
-    ['Företagssäljare'],
-    ['Vårdbiträden'],
-    ['Städare'],
-    ['Vårdare, boendestödjare'],
-    ['Övriga kontorsassistenter och sekreterare'],
-    ['Personliga assistenter'],
-    ['Förskollärare'],
-    ['Restaurang- och köksbiträden m.fl.'],
-    ['Planerare och utredare m.fl.'],
-    ['Lastbilsförare m.fl.'],
-    ['Grundutbildade sjuksköterskor'],
-    ['Maskinställare och maskinoperatörer, metallarbete'],
-    ['Träarbetare, snickare m.fl.'],
-    ['Fastighetsskötare'],
-    ['Elevassistenter m.fl.'],
-    ['Lednings- och organisationsutvecklare'],
-    ['Kockar och kallskänkor'],
-    ['Undersköterskor, vård- och specialavdelning'],
-    ['Ekonomiassistenter m.fl.'],
-    ['Installations- och serviceelektriker'],
-    ['Kundtjänstpersonal'],
-    ['Motorfordonsmekaniker och fordonsreparatörer'],
-    ['Ingenjörer och tekniker inom elektroteknik']
+# Define the list of job titles
+job_titles = [
+    'Undersköterskor, hemtjänst, hemsjukvård och äldreboende',
+    'Grundskollärare',
+    'Butikssäljare, fackhandel',
+    'Lager- och terminalpersonal',
+    'Mjukvaru- och systemutvecklare m.fl.',
+    'Barnskötare',
+    'Butikssäljare, dagligvaror',
+    'Företagssäljare',
+    'Vårdbiträden',
+    'Städare',
+    'Vårdare, boendestödjare',
+    'Övriga kontorsassistenter och sekreterare',
+    'Personliga assistenter',
+    'Förskollärare',
+    'Restaurang- och köksbiträden m.fl.',
+    'Planerare och utredare m.fl.',
+    'Lastbilsförare m.fl.',
+    'Grundutbildade sjuksköterskor',
+    'Maskinställare och maskinoperatörer, metallarbete',
+    'Träarbetare, snickare m.fl.',
+    'Fastighetsskötare',
+    'Elevassistenter m.fl.',
+    'Lednings- och organisationsutvecklare',
+    'Kockar och kallskänkor',
+    'Undersköterskor, vård- och specialavdelning',
+    'Ekonomiassistenter m.fl.',
+    'Installations- och serviceelektriker',
+    'Kundtjänstpersonal',
+    'Motorfordonsmekaniker och fordonsreparatörer',
+    'Ingenjörer och tekniker inom elektroteknik'
 ]
+
+# Define the search function
+def search_job_titles(query):
+    matches = [title for title in job_titles if query.lower() in title.lower()]
+    return matches
+
+# Search for job titles based on user input
+search_query = st.sidebar.text_input("Search Job Titles")
+search_results = search_job_titles(search_query)
+
+# Display the search results
+st.sidebar.markdown("### Search Results")
+for result in search_results:
+    st.sidebar.write(result)
 
 # Calculate the similarity score and buzzword count
 if job_ad_text:
-    similarity_score = get_cosine_similarity_score(job_ad_text, ' '.join([item[0] for item in data]))
-    buzzword_count = sum([1 for word in [item[0].lower() for item in data] if word in job_ad_text.lower()])
+    similarity_score = get_cosine_similarity_score(job_ad_text, ' '.join(buzzwords))
+    buzzword_count = sum([1 for word in buzzwords if word in job_ad_text.lower()])
 else:
     similarity_score = 0
     buzzword_count = 0
-
-# Search for matches in the list
-if search_input:
-    search_results = search_keywords(search_input)
-else:
-    search_results = []
-
-# Display the search results
-st.markdown("### Search Results")
-if search_results:
-    for result in search_results:
-        st.write(result)
-else:
-    st.write("No matching results found.")
 
 # Display the results
 st.markdown(f"### Results for {industry}")
