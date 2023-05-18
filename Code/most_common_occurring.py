a0f50042475619625415053a7139359fccbe3293
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-05-18 10:46:06 +0200

    Update most_common_occurring.py
    Updated the code so that one can search for monograms, bigrams or both

--- Code/most_common_occurring.py
+++ Code/most_common_occurring.py
@@ -1,8 +1,9 @@
 from collections import Counter
 from nltk import ngrams
 import pandas as pd
+import re
 
-data = pd.read_csv('C:/Users/carlt/Documents/TIG326/kod till projektet/assigna-group3/code/preprocessed_swe_1.csv', index_col=0)
+data = pd.read_csv('C:/Users/jonat/Documents/GitHub/assigna-group3/Code/preprocessed_swe_1.csv', index_col=0)
 
 def get_top_grams(df, row_limit, num_grams):
     # Limit the DataFrame to the specified number of rows
@@ -13,14 +14,29 @@ def get_top_grams(df, row_limit, num_grams):
 
     # Extract monograms and bigrams from tokenized descriptions
     for desc in df['description']:
-        grams = desc.split()
+        words = re.findall(r'\w+', desc.lower())                 #<-----  
+        #grams = list(ngrams(words, 1)) + list(ngrams(words, 2))  #<----- Denna gör att man får ut bigrams och monograms, men den lägger alla inom paranteser
+        grams =list(ngrams(words, 2))                           #<------ Denna tar bara ut bigrams
+        #grams = desc.split()                                    #<----- Dennna tar bara ut monograms
         all_grams.extend(grams)
     
-    excluded_words = ['arbet', 'kund', 'sök', 'hos', 'tjänst', 'topwork', 'komm', 'medarbet', 'person', 'nya', 'erfaren', 'god', 'rätt', 'utveckl', 'roll', 'anställning', 'vill', 'ska', 'får', 'vikt', 'jobb', 'samt', 'båd', 'finn', 'krav', 'företag', 'kunskap', 'svensk', 'utmaning', 'tid', 'möj', 'mål', 'uppdrag', 'stor', 'hitt', 'körkort', 'rekrytering', 'trygg', 'bygg', 'erbjud', 'även', 'b', 'in', 'konsult', 'stött', 'sen', 'del', 'oavset', 'arbetsuppgift', 'meriter']
+    #Exkluderade ord, funkar bara om man söker efter monograms
+    #Bigrams verkar dock mer intressanta
+    excluded_words = [
+       
+                      'arbet', 'kund', 'sök', 'hos', 'tjänst', 'topwork', 'komm', 'medarbet', 'person', 'nya', 'erfaren', 'god', 
+                      'rätt', 'utveckl', 'roll', 'anställning', 'vill', 'ska', 'får', 'vikt', 'jobb', 'samt', 'båd', 'finn', 'krav',      
+                     'företag', 'kunskap', 'svensk', 'utmaning', 'tid', 'möj', 'mål', 'uppdrag', 'stor', 'hitt', 'körkort', 'rekrytering',
+                     'trygg', 'bygg', 'erbjud', 'även', 'b', 'in', 'konsult', 'stött', 'sen', 'del', 'oavset', 'arbetsuppgift', 'meriter'
+                     'gärn','ta','kräv','ser','triv','råd','övergå','tal','ger', 'lägg','dag', 'övr','–','idag','kunn','start','redan',   
+                     'genom','hjälp','gill', 'såväl','tyck','sver','högt','få','sätt','andr', 'se','mer', 'enl','hel','via', 'www','år', 
+                      'också','går','väx', '•','tillsamman','gör','olik', 'när', 'stockholm','patient','vid','engelsk', 'ge','organisation',
+                      'lyck', 'därför', 'skrift','hög', 'sjukskötersk', 'https','läs','följ','elev', 'verksam', 'löp', '·','region','lön','fråg',
+                                                               
+                     ]
 
 # Remove excluded words from all_grams
     all_grams = [gram for gram in all_grams if not any(word in gram for word in excluded_words)]
-
 
     # Count the occurrences of each monogram and bigram
     gram_counts = Counter(all_grams)
@@ -30,7 +46,9 @@ def get_top_grams(df, row_limit, num_grams):
 
     return top_grams
 
-top_grams = get_top_grams(data, 200, 50)
+top_grams = get_top_grams(data, 20000, 300)
+for gram, count in top_grams:
+    print(gram, ":", count)
 for gram, count in top_grams:
     print(gram, end='')
 
@@ -52,3 +70,5 @@ top_grams = get_top_grams(df, row_limit, num_grams)
 for gram, count in top_grams:
     print(gram, ":", count)
 '''
+
+
