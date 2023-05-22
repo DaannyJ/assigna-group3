bc66ef22d2cf3db3b2fc3536ba5befc144437e99
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-05-22 16:02:09 +0200

    updated cosine again, can exclude words aswell

--- Code/cosine.py
+++ Code/cosine.py
@@ -60,24 +60,51 @@ from buzz_words_list import *
 import numpy as np
 from colorama import Fore, Style
 from termcolor import colored
+from excluded_words_list import *
 
 df = pd.read_csv('C:/Users/jonat/Documents/GitHub/assigna-group3/Code/preprocessed_swe_1.csv')
 pd.set_option('display.max_colwidth', 100)
 
+
+
 mono_buzz = buzz_monograms()
 bi_buzz = buzz_bigrams()
-
+'''
 # Combine the descriptions and word list
 descriptions = df['description'].tolist()
 word_list_strings = [' '.join(tup) for tup in bi_buzz]
-word_list = mono_buzz + word_list_strings
-documents = descriptions + word_list
+word_list = word_list_strings + mono_buzz
+#print(word_list)
 
+excluded_words = excluded_monograms()
+excluded_combinations = excluded_bigrams()
+
+documents = descriptions + word_list
+'''
+excluded_words = excluded_monograms()
+excluded_combinations = excluded_bigrams()
+
+# Combine the descriptions and word list
+descriptions = df['description'].tolist()
+
+# Exclude words from descriptions
+filtered_descriptions = []
+for description in descriptions:
+    description_words = description.split()
+    filtered_words = [word for word in description_words if word not in excluded_words and word not in excluded_combinations]
+    filtered_description = ' '.join(filtered_words)
+    filtered_descriptions.append(filtered_description)
+
+word_list_strings = [' '.join(tup) for tup in excluded_combinations]
+word_list = excluded_words + word_list_strings
+documents = filtered_descriptions + word_list_strings
 vectorizer = TfidfVectorizer()
 vectorized_documents = vectorizer.fit_transform(documents)
 
 # Calculate similarity between descriptions and word list
 similarity_descriptions_word_list = cosine_similarity(vectorized_documents[:len(descriptions)], vectorized_documents[len(descriptions):])
+
+
 
 # Extract similarity scores for each description
 description_scores = similarity_descriptions_word_list.max(axis=1)
