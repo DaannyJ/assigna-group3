dd7812b13d6c54025167d20e6904b68e1c60bf93
casamu <gussamcag@student.gu.se>
2023-05-23 15:51:42 +0200

    Removed unnecessary imports

--- Code/preprocessor.py
+++ Code/preprocessor.py
@@ -1,8 +1,5 @@
 ### HELLO!!
 ### This is code that removes special characters and numbers, tokenizes, stems, and removes stopwords from a column in a csv 
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 ## Removing punctuation, numbers. Tokenization, stemming and stopword removal
 def preprocess_swedish_text(text):
 
