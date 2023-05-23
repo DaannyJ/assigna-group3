89f6c5d926b99a5dac66ebfb3560c6c6821c4447
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-05-23 15:46:08 +0200

    Update cosine_1.py

--- Code/cosine_1.py
+++ Code/cosine_1.py
@@ -14,19 +14,21 @@ def get_avg_buzz():
     avg_buzz = average_score
     return avg_buzz
 
+def get_both():
+    both = documents
+    return both
 
 
 
 
-
-df = pd.read_csv('C:/Users/danie/Documents/assigna-group3-1/Code/preprocessed_swe_1.csv')
+df = pd.read_csv('C:/Users/jonat/Documents/GitHub/assigna-group3/Code/preprocessed_swe_1.csv')
 pd.set_option('display.max_colwidth', 100)
 
 mono_buzz = buzz_monograms()
 bi_buzz = buzz_bigrams()
 
-excluded_words = excluded_monograms()
-excluded_combinations = excluded_bigrams()
+excluded_words = [] #excluded_monograms()
+excluded_combinations = [('thanos','magoulas')]#excluded_bigrams()
 
 # Combine the descriptions and word list
 descriptions = df['description'].tolist()
@@ -42,7 +44,6 @@ for description in descriptions:
 word_list_strings = [' '.join(tup) for tup in bi_buzz]
 word_list = word_list_strings + mono_buzz
 documents = filtered_descriptions + word_list
-
 vectorizer = TfidfVectorizer()
 vectorized_documents = vectorizer.fit_transform(documents)
 
@@ -51,14 +52,14 @@ similarity_descriptions_word_list = cosine_similarity(vectorized_documents[:len(
 
 # Extract similarity scores for each description
 description_scores = similarity_descriptions_word_list.max(axis=1)
-print(type(description_scores))
-print(description_scores)
+#print(type(description_scores))
+#print(description_scores)
 # Calculate the average score of all descriptions
 average_score = np.mean(description_scores)
 
 # Get the maximum length of headline for formatting purposes
 max_headline_length = max(len(headline) for headline in df['headline'])
-'''
+
 print("Description Index\tHeadline" + " "*(max_headline_length-8) + "\t\tSimilarity Score")
 print("--------------------------------------------------------------")
 for i, (headline, score) in enumerate(zip(df['headline'], description_scores), 1):
@@ -73,7 +74,7 @@ selected_description = df.iloc[selected_index]['description']
 print("\nSelected description:")
 print(selected_description)
 
-'''
+
 
 #print(get_buzz())
 
